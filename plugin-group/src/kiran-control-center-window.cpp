2385dd512c273f4f023a8b220c595e2fd6eed237
liuxinhao <liuxinhao@kylinsec.com.cn>
2021-03-30 16:37:10 +0800

    refactor(*): replace KiranSearchBox with kiran-search-lineedit. - 由于kiranwidgets-qt5中搜索框的部分功能未实现，且暂不确定后续是否会封装这些功能，所以用自定义控件kiran-search-lineedit替换掉KiranSearchBox.

--- plugin-group/src/kiran-control-center-window.cpp
+++ plugin-group/src/kiran-control-center-window.cpp
@@ -1,5 +1,6 @@
 #include "kiran-control-center-window.h"
-#include "kiranwidgets-qt5/kiran-search-box.h"
+//#include "kiranwidgets-qt5/kiran-search-box.h"
+#include "kiran-search-lineedit/kiran-search-lineedit.h"
 #include <QHBoxLayout>
 #include <QCompleter>
 #include <QIcon>
@@ -19,19 +20,19 @@ KiranControlCenterWindow::KiranControlCenterWindow(QWidget *parent) : KiranTitle
     setWindowContentWidget(centerWgt);
 
     QHBoxLayout *layout = getTitlebarCustomLayout();
-//    KiranSearchLineEdit *search = new KiranSearchLineEdit(centerWgt->completerKeys(), this);
-//    layout->addWidget(search);
-//    //绑定搜索槽函数
-//    connect(search, SIGNAL(sigSearch(QString)), centerWgt, SLOT(onSearch(QString)));
-    KiranSearchBox *search = new KiranSearchBox(this);
-    search->setFixedSize(352, 40);
+    KiranSearchLineEdit *search = new KiranSearchLineEdit(centerWgt->completerKeys(), this);
     layout->addWidget(search);
-    QCompleter *completer = new QCompleter(centerWgt->completerKeys(), this);
-    //设置任意位置匹配
-    completer->setFilterMode(Qt::MatchContains);
-    completer->setCaseSensitivity(Qt::CaseSensitive);
-    search->setCompleter(completer);
-    connect(completer, SIGNAL(activated(QString)), centerWgt, SLOT(onSearch(QString)));
+    //绑定搜索槽函数
+    connect(search, SIGNAL(sigSearch(QString)), centerWgt, SLOT(onSearch(QString)));
+//    KiranSearchBox *search = new KiranSearchBox(this);
+//    search->setFixedSize(352, 40);
+//    layout->addWidget(search);
+//    QCompleter *completer = new QCompleter(centerWgt->completerKeys(), this);
+//    //设置任意位置匹配
+//    completer->setFilterMode(Qt::MatchContains);
+//    completer->setCaseSensitivity(Qt::CaseSensitive);
+//    search->setCompleter(completer);
+//    connect(completer, SIGNAL(activated(QString)), centerWgt, SLOT(onSearch(QString)));
 
     resize(1060, 830);
 }
