2ee817a99294267cc061a5ccff906d966601b541
liuxinhao <liuxinhao@kylinsec.com.cn>
2021-04-27 10:51:11 +0800

    refactor(*): cleaning up useless compilation intermediate files
    - 清理无用的编译中间文件

--- cmake-build-debug/CMakeFiles/kiran_power_manager_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/kiran_power_manager_autogen"
-  "kiran_power_manager_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/kiran_power_manager_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/config.h
+++
@@ -1,7 +0,0 @@
-#ifndef __POWER_MANAGER_CONFIG_H__
-#define __POWER_MANAGER_CONFIG_H__
-
-#define KIRAN_POWER_MANAGER_TRANSLATIONS_DIR "/usr/local/share/kiran_power_manager/translations"
-#define KIRAN_POWER_MANAGER_DESKTOP_ICON ""
-
-#endif
