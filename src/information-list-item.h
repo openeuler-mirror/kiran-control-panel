6daf8e7ac342c75e4b62049a290f9bc7223dce45
liuxinhao <liuxinhao@kylinsec.com.cn>
2021-07-15 14:30:58 +0800

    docs(*):add license information in to code
    - 在代码在添加版权信息

--- src/information-list-item.h
+++ src/information-list-item.h
@@ -1,8 +1,20 @@
 /**
- * @file information-list-item.h
- * @brief 系统信息列表
- * @author yuanxing@kylinos.com.cn
- * @copyright Copyright ©2020 KylinSec. All rights reserved.
+ * @Copyright (C) 2020 ~ 2021 KylinSec Co., Ltd.
+ *
+ * Author:     yuanxing <yuanxing@kylinos.com.cn>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; If not, see <http: //www.gnu.org/licenses/>.
  */
 
 #ifndef INFORMATIONLISTITEM_H
