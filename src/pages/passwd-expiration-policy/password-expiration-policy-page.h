6fb07e7afe7bc31ff5155b47d0be88491f2d098a
liuxinhao <liuxinhao@kylinsec.com.cn>
2021-08-09 11:37:51 +0800

    style(license):modify the license
    - 在新增源文件的头部注释中添加木兰协议

--- src/pages/passwd-expiration-policy/password-expiration-policy-page.h
+++ src/pages/passwd-expiration-policy/password-expiration-policy-page.h
@@ -1,6 +1,16 @@
-//
-// Created by lxh on 2021/8/7.
-//
+/**
+ * Copyright (c) 2020 ~ 2021 KylinSec Co., Ltd.
+ * kiran-cpanel-account is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ *
+ * Author:     liuxinhao <liuxinhao@kylinos.com.cn>
+ */
 
 #ifndef KIRAN_CPANEL_ACCOUNT_SRC_PAGES_PASSWD_EXPIRATION_POLICY_PASSWORD_EXPIRATION_POLICY_PAGE_H_
 #define KIRAN_CPANEL_ACCOUNT_SRC_PAGES_PASSWD_EXPIRATION_POLICY_PASSWORD_EXPIRATION_POLICY_PAGE_H_
