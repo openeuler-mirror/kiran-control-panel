f6dab3f9924a3df346cf52cd05df8d0e128a22b0
liuxinhao <liuxinhao@kylinsec.com.cn>
2021-08-26 16:06:58 +0800

    fix(translator): update translator
    - 更新翻译
    
    Signed-off-by: liuxinhao <liuxinhao@kylinsec.com.cn>

--- avatar-editor/translations/kiran-avatar-editor.zh_CN.ts
+++ avatar-editor/translations/kiran-avatar-editor.zh_CN.ts
@@ -1,30 +1,22 @@
-< ? xml version = "1.0"
-encoding = "utf-8" ?
->
-<!DOCTYPE
-TS >
-<TS version = "2.1"
-language = "zh_CN" >
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
 <context>
-    <name>KiranAvatarEditor < /name>
-< message >
-<location filename = "../src/kiran-avatar-editor.cpp"
-line = "40" / >
-    <source>Avatar
-Editor < /source>
-< translation > 头像编辑器 < /translation>
-< /message>
-< message >
-<location filename = "../src/kiran-avatar-editor.cpp"
-line = "87" / >
-<source>Confirm < /source>
-< translation > 确认 < /translation>
-< /message>
-< message >
-<location filename = "../src/kiran-avatar-editor.cpp"
-line = "108" / >
-    <source>Cancel < /source>
-    < translation > 取消 < /translation>
-    < /message>
-    < /context>
-    < /TS>
+    <name>KiranAvatarEditor</name>
+    <message>
+        <location filename="../src/kiran-avatar-editor.cpp" line="40"/>
+        <source>Avatar Editor</source>
+        <translation>头像编辑器</translation>
+    </message>
+    <message>
+        <location filename="../src/kiran-avatar-editor.cpp" line="87"/>
+        <source>Confirm</source>
+        <translation>确认</translation>
+    </message>
+    <message>
+        <location filename="../src/kiran-avatar-editor.cpp" line="108"/>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+</context>
+</TS>
