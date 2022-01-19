afb7517ddc234e5c0ed8f58ad796d2d5fc47369e
liuxinhao <liuxinhao@kylinsec.com.cn>
2022-01-19 09:18:40 +0800

    fix(brightness): abnormal settings brightness
    - 修复亮度设置不正常的问题
    
    Closes #48486

--- src/pages/general-settings/general-settings-page.cpp
+++ src/pages/general-settings/general-settings-page.cpp
@@ -324,13 +324,14 @@ void GeneralSettingsPage::setBrightnessPercent(int percent)
     if (percent < 0)
     {
         ui->slider_brightness->setEnabled(false);
+        m_brightnessValue = 0;
         ui->slider_brightness->setValue(0);
         ui->label_brightnessPercent->setText(tr("brightness adjustment not available"));
     }
     else
     {
         ui->slider_brightness->setEnabled(true);
-        ui->slider_brightness->setValue(percent);
+        m_brightnessValue = percent;
         ui->label_brightness->setText(QString("%1%").arg(percent));
     }
 }
