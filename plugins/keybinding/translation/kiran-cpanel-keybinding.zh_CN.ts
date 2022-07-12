047b1201db573f2899d3f4a8ccefcd17b1e877c1
liuxinhao <liuxinhao@kylinsec.com.cn>
2022-07-12 14:50:52 +0800

    fix(translator): update translator
        - 更新控制中心部分翻译

--- plugins/keybinding/translation/kiran-cpanel-keybinding.zh_CN.ts
+++ plugins/keybinding/translation/kiran-cpanel-keybinding.zh_CN.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../src/keycode-translator.cpp" line="128"/>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>暂无</translation>
     </message>
     <message>
         <location filename="../src/keycode-translator.cpp" line="132"/>
         <source>disabled</source>
-        <translation type="unfinished"></translation>
+        <translation>禁用</translation>
     </message>
 </context>
 <context>
@@ -19,87 +19,87 @@
     <message>
         <location filename="../src/keycode-translator.cpp" line="57"/>
         <source>Audio Play</source>
-        <translation type="unfinished"></translation>
+        <translation>音频播放</translation>
     </message>
     <message>
         <location filename="../src/keycode-translator.cpp" line="58"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>搜索</translation>
     </message>
     <message>
         <location filename="../src/keycode-translator.cpp" line="59"/>
         <source>WWW</source>
-        <translation type="unfinished"></translation>
+        <translation>万维网</translation>
     </message>
     <message>
         <location filename="../src/keycode-translator.cpp" line="60"/>
         <source>Audio Lower Volume</source>
-        <translation type="unfinished"></translation>
+        <translation>减小音量</translation>
     </message>
     <message>
         <location filename="../src/keycode-translator.cpp" line="61"/>
         <source>Audio Raise Volume</source>
-        <translation type="unfinished"></translation>
+        <translation>增大音量</translation>
     </message>
     <message>
         <location filename="../src/keycode-translator.cpp" line="62"/>
         <source>Mic Mute</source>
-        <translation type="unfinished"></translation>
+        <translation>输入静音</translation>
     </message>
     <message>
         <location filename="../src/keycode-translator.cpp" line="63"/>
         <source>Audio Stop</source>
-        <translation type="unfinished"></translation>
+        <translation>音频停止</translation>
     </message>
     <message>
         <location filename="../src/keycode-translator.cpp" line="64"/>
         <source>Explorer</source>
-        <translation type="unfinished"></translation>
+        <translation>浏览</translation>
     </message>
     <message>
         <location filename="../src/keycode-translator.cpp" line="65"/>
         <source>Calculator</source>
-        <translation type="unfinished"></translation>
+        <translation>计算器</translation>
     </message>
     <message>
         <location filename="../src/keycode-translator.cpp" line="66"/>
         <source>Audio Mute</source>
-        <translation type="unfinished"></translation>
+        <translation>音频暂停</translation>
     </message>
     <message>
         <location filename="../src/keycode-translator.cpp" line="67"/>
         <source>Audio Pause</source>
-        <translation type="unfinished"></translation>
+        <translation>音频暂停</translation>
     </message>
     <message>
         <location filename="../src/keycode-translator.cpp" line="68"/>
         <source>Audio Prev</source>
-        <translation type="unfinished"></translation>
+        <translation>音频上一个</translation>
     </message>
     <message>
         <location filename="../src/keycode-translator.cpp" line="69"/>
         <source>Audio Media</source>
-        <translation type="unfinished"></translation>
+        <translation>音频媒体</translation>
     </message>
     <message>
         <location filename="../src/keycode-translator.cpp" line="70"/>
         <source>Audio Next</source>
-        <translation type="unfinished"></translation>
+        <translation>音频下一个</translation>
     </message>
     <message>
         <location filename="../src/keycode-translator.cpp" line="71"/>
         <source>Mail</source>
-        <translation type="unfinished"></translation>
+        <translation>邮件</translation>
     </message>
     <message>
         <location filename="../src/keycode-translator.cpp" line="72"/>
         <source>Tools</source>
-        <translation type="unfinished"></translation>
+        <translation>工具</translation>
     </message>
     <message>
         <location filename="../src/keycode-translator.cpp" line="73"/>
         <source>Eject</source>
-        <translation type="unfinished"></translation>
+        <translation>弹出</translation>
     </message>
 </context>
 <context>
@@ -107,201 +107,201 @@
     <message>
         <location filename="../src/shortcut.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../src/shortcut.ui" line="172"/>
+        <location filename="../src/shortcut.ui" line="175"/>
         <source>Custom</source>
-        <translation type="unfinished"></translation>
+        <translation>自定义</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.ui" line="204"/>
-        <location filename="../src/shortcut.cpp" line="156"/>
-        <location filename="../src/shortcut.cpp" line="548"/>
+        <location filename="../src/shortcut.ui" line="207"/>
+        <location filename="../src/shortcut.cpp" line="158"/>
+        <location filename="../src/shortcut.cpp" line="550"/>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>编辑</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.ui" line="334"/>
-        <location filename="../src/shortcut.ui" line="524"/>
-        <location filename="../src/shortcut.cpp" line="102"/>
-        <location filename="../src/shortcut.cpp" line="113"/>
+        <location filename="../src/shortcut.ui" line="340"/>
+        <location filename="../src/shortcut.ui" line="533"/>
+        <location filename="../src/shortcut.cpp" line="104"/>
+        <location filename="../src/shortcut.cpp" line="115"/>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>添加</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.ui" line="366"/>
+        <location filename="../src/shortcut.ui" line="375"/>
         <source>Reset</source>
-        <translation type="unfinished"></translation>
+        <translation>重置</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.ui" line="412"/>
+        <location filename="../src/shortcut.ui" line="421"/>
         <source>Custom Shortcut Name</source>
-        <translation type="unfinished"></translation>
+        <translation>自定义快捷键名称</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.ui" line="442"/>
+        <location filename="../src/shortcut.ui" line="451"/>
         <source>Custom Shortcut application</source>
-        <translation type="unfinished"></translation>
+        <translation>自定义快捷键应用程序</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.ui" line="472"/>
+        <location filename="../src/shortcut.ui" line="481"/>
         <source>Custom Shortcut Key</source>
-        <translation type="unfinished"></translation>
+        <translation>自定义快捷键</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.ui" line="556"/>
+        <location filename="../src/shortcut.ui" line="568"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>取消</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.ui" line="602"/>
+        <location filename="../src/shortcut.ui" line="614"/>
         <source>Shortcut Name</source>
-        <translation type="unfinished"></translation>
+        <translation>快捷键名称</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.ui" line="645"/>
+        <location filename="../src/shortcut.ui" line="657"/>
         <source>Shortcut application</source>
-        <translation type="unfinished"></translation>
+        <translation>快捷键应用程序</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.ui" line="676"/>
+        <location filename="../src/shortcut.ui" line="688"/>
         <source>Shortcut key</source>
-        <translation type="unfinished"></translation>
+        <translation>快捷键</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.ui" line="728"/>
+        <location filename="../src/shortcut.ui" line="740"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>保存</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.ui" line="760"/>
+        <location filename="../src/shortcut.ui" line="775"/>
         <source>return</source>
-        <translation type="unfinished"></translation>
+        <translation>返回</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.cpp" line="84"/>
+        <location filename="../src/shortcut.cpp" line="86"/>
         <source>Please enter a search keyword...</source>
-        <translation type="unfinished"></translation>
+        <translation>请输入搜索关键字...</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.cpp" line="96"/>
+        <location filename="../src/shortcut.cpp" line="98"/>
         <source>Required</source>
-        <translation type="unfinished"></translation>
+        <translation>必填</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.cpp" line="122"/>
-        <location filename="../src/shortcut.cpp" line="128"/>
+        <location filename="../src/shortcut.cpp" line="124"/>
+        <location filename="../src/shortcut.cpp" line="130"/>
         <source>Please press the new shortcut key</source>
-        <translation type="unfinished"></translation>
+        <translation>请输入新快捷键</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.cpp" line="154"/>
+        <location filename="../src/shortcut.cpp" line="156"/>
         <source>Finished</source>
-        <translation type="unfinished"></translation>
+        <translation>完成</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.cpp" line="652"/>
-        <location filename="../src/shortcut.cpp" line="702"/>
-        <location filename="../src/shortcut.cpp" line="723"/>
-        <location filename="../src/shortcut.cpp" line="758"/>
-        <location filename="../src/shortcut.cpp" line="778"/>
-        <location filename="../src/shortcut.cpp" line="802"/>
-        <location filename="../src/shortcut.cpp" line="822"/>
+        <location filename="../src/shortcut.cpp" line="654"/>
+        <location filename="../src/shortcut.cpp" line="704"/>
+        <location filename="../src/shortcut.cpp" line="725"/>
+        <location filename="../src/shortcut.cpp" line="760"/>
+        <location filename="../src/shortcut.cpp" line="780"/>
+        <location filename="../src/shortcut.cpp" line="804"/>
+        <location filename="../src/shortcut.cpp" line="824"/>
         <source>Failed</source>
-        <translation type="unfinished"></translation>
+        <translation>失败</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.cpp" line="297"/>
+        <location filename="../src/shortcut.cpp" line="299"/>
         <source>Get shortcut failed,error:</source>
-        <translation type="unfinished"></translation>
+        <translation>获取快捷键失败，错误：</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.cpp" line="209"/>
+        <location filename="../src/shortcut.cpp" line="211"/>
         <source>failed to load shortcut key data!</source>
-        <translation type="unfinished"></translation>
+        <translation>加载快捷键数据失败！</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.cpp" line="251"/>
+        <location filename="../src/shortcut.cpp" line="253"/>
         <source>List shortcut failed,error:%1</source>
-        <translation type="unfinished"></translation>
+        <translation>列出快捷键失败，错误:%1</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.cpp" line="296"/>
+        <location filename="../src/shortcut.cpp" line="298"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>错误</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.cpp" line="370"/>
+        <location filename="../src/shortcut.cpp" line="372"/>
         <source>Open File</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/shortcut.cpp" line="600"/>
-        <source>System</source>
-        <translation type="unfinished"></translation>
+        <translation>打开文件</translation>
     </message>
     <message>
         <location filename="../src/shortcut.cpp" line="602"/>
+        <source>System</source>
+        <translation>系统</translation>
+    </message>
+    <message>
+        <location filename="../src/shortcut.cpp" line="604"/>
         <source>Sound</source>
-        <translation type="unfinished"></translation>
+        <translation>声音</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.cpp" line="653"/>
+        <location filename="../src/shortcut.cpp" line="655"/>
         <source>Delete shortcut failed,error:</source>
-        <translation type="unfinished"></translation>
+        <translation>删除快捷键失败，错误：</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.cpp" line="666"/>
-        <location filename="../src/shortcut.cpp" line="741"/>
+        <location filename="../src/shortcut.cpp" line="668"/>
+        <location filename="../src/shortcut.cpp" line="743"/>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation>警告</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.cpp" line="667"/>
-        <location filename="../src/shortcut.cpp" line="742"/>
+        <location filename="../src/shortcut.cpp" line="669"/>
+        <location filename="../src/shortcut.cpp" line="744"/>
         <source>Please complete the shortcut information!</source>
-        <translation type="unfinished"></translation>
+        <translation>请完善快捷键信息！</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.cpp" line="676"/>
+        <location filename="../src/shortcut.cpp" line="678"/>
         <source>Set shortcut</source>
-        <translation type="unfinished"></translation>
+        <translation>设置快捷键</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.cpp" line="677"/>
+        <location filename="../src/shortcut.cpp" line="679"/>
         <source>Are you sure you want to disable this shortcut?</source>
-        <translation type="unfinished"></translation>
+        <translation>是否确定要禁用此快捷键？</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.cpp" line="703"/>
+        <location filename="../src/shortcut.cpp" line="705"/>
         <source>Modify system shortcut failed,error:</source>
-        <translation type="unfinished"></translation>
+        <translation>修改系统快捷键失败，错误：</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.cpp" line="724"/>
+        <location filename="../src/shortcut.cpp" line="726"/>
         <source>Modify custom shortcut failed,error:</source>
-        <translation type="unfinished"></translation>
+        <translation>修改自定义快捷键失败，错误：</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.cpp" line="759"/>
+        <location filename="../src/shortcut.cpp" line="761"/>
         <source>Add custom shortcut failed,error:</source>
-        <translation type="unfinished"></translation>
+        <translation>添加自定义快捷键失败，错误：</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.cpp" line="779"/>
+        <location filename="../src/shortcut.cpp" line="781"/>
         <source>Reset shortcut failed,error:</source>
-        <translation type="unfinished"></translation>
+        <translation>重置快捷键失败，错误：</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.cpp" line="803"/>
+        <location filename="../src/shortcut.cpp" line="805"/>
         <source>Cannot use shortcut &quot;%1&quot;, Because you cannot enter with this key.Please try again using Ctrl, Alt, or Shift at the same time.</source>
-        <translation type="unfinished"></translation>
+        <translation>无法使用快捷键&quot;%1&quot;,因为使用此键将无法输入，请同时使用Ctrl，Alt，Shift再试一次。</translation>
     </message>
     <message>
-        <location filename="../src/shortcut.cpp" line="823"/>
+        <location filename="../src/shortcut.cpp" line="825"/>
         <source>Shortcut keys %1 are already used in %2,Please try again!</source>
-        <translation type="unfinished"></translation>
+        <translation>快捷键%1已用于%2，请再试一次！</translation>
     </message>
 </context>
 <context>
@@ -309,13 +309,13 @@
     <message>
         <location filename="../src/shortcut-item.ui" line="32"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/shortcut-item.ui" line="53"/>
         <location filename="../src/shortcut-item.ui" line="73"/>
         <source>TextLabel</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -323,12 +323,12 @@
     <message>
         <location filename="../src/thread-object.cpp" line="134"/>
         <source>Failed</source>
-        <translation type="unfinished"></translation>
+        <translation>失败</translation>
     </message>
     <message>
         <location filename="../src/thread-object.cpp" line="135"/>
         <source>List shortcut failed,error:</source>
-        <translation type="unfinished"></translation>
+        <translation>列出快捷键失败，错误：</translation>
     </message>
 </context>
 </TS>
