d49781db1925919518878aff0d7863d7261a334e
yuanxing <yuanxing@kylinos.com.cn>
2023-08-11 17:10:10 +0800

    fix(keyboard):change describtion of enter repeat chars to test
    - 修改键盘设置描述:输入重复按键测试
    
    Fixes #7581

--- plugins/keyboard/translation/kiran-cpanel-keyboard.zh_CN.ts
+++ plugins/keyboard/translation/kiran-cpanel-keyboard.zh_CN.ts
@@ -84,8 +84,8 @@
     <message>
         <location filename="../src/pages/general/general-page.ui" line="338"/>
         <location filename="../src/pages/general/general-page.cpp" line="54"/>
-        <source>Enter characters to test the settings</source>
-        <translation>输入字符来测试设置</translation>
+        <source>Enter repeat characters to test</source>
+        <translation>输入重复字符测试</translation>
     </message>
     <message>
         <location filename="../src/pages/general/general-page.ui" line="357"/>
