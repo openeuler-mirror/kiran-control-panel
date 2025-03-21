0a1e7c5fd7b1b66919515c40d191710472de8858
yuanxing <yuanxing@kylinsec.com.cn>
2025-03-21 15:15:13 +0800

    fix(appearance):compatible with different versions of open-source cursor to prevent program crashes
    - 兼容不同版本的开源光标，防止程序崩溃
    
    Fixes #IBV1XG

--- plugins/appearance/pages/theme/cursor/cursor-theme-page.cpp
+++ plugins/appearance/pages/theme/cursor/cursor-theme-page.cpp
@@ -28,7 +28,14 @@
 #include "exclusion-group.h"
 #include "theme-preview-widget.h"
 
-QStringList CursorThemePage::m_previewCursors = {"left_ptr", "right_ptr", "top_left_corner", "top_right_corner", "size_hor", "pointer"};
+// 兼容不同开源版本光标名称，若光标名不存在，则使用默认光标
+static const std::vector<const char*> cursorNames[] = {
+    {"left_ptr", "default", "top_left_arrow", "left_arrow"},
+    {"pointing_hand", "pointer", "hand1", "e29285e634086352946a0e7090d73106", "default"},
+    {"size_ver", "ns-resize", "v_double_arrow", "00008160000006810000408080010102", "default"},
+    {"size_hor", "ew-resize", "h_double_arrow", "028006030e0e7ebffc7f7070c0600140", "default"},
+    {"size_bdiag", "nesw-resize", "50585d75b494802d0151028115016902", "fcf1c3c7cd4491d801f1e1c78f100000", "default"},
+    {"size_fdiag", "nwse-resize", "38c5dff7c7b8962045400281044508d2", "c7088f0f3e6c8088236ef8e1e3e70000", "default"}};
 
 CursorThemePage::CursorThemePage(QWidget* parent) : QWidget(parent)
 {
@@ -56,7 +63,7 @@ void CursorThemePage::initUI()
     layout->addWidget(label_text);
 
     m_exclusionGroup = new ExclusionGroup(this);
-    connect(m_exclusionGroup,&ExclusionGroup::currentItemChanged,this,&CursorThemePage::onCurrentItemChanged);
+    connect(m_exclusionGroup, &ExclusionGroup::currentItemChanged, this, &CursorThemePage::onCurrentItemChanged);
 
     QWidget* cursorsContainer = new QWidget(this);
     m_cursorVlayout = new QVBoxLayout(cursorsContainer);
@@ -81,14 +88,22 @@ void CursorThemePage::loadCurosrThemes()
         const QString& themeBaseName = cursorThemeDir.dirName();
 
         QList<QPixmap> themeCursors;
-        for (auto cursorName : m_previewCursors)
+        for (auto cursors : cursorNames)
         {
-            auto cursorImg = CursorImageLoader::getCursorImage(themeBaseName, cursorName, 22);
-            auto curosrPixmap = QPixmap::fromImage(cursorImg);
-            themeCursors << curosrPixmap;
+            for (auto cursorName : cursors)
+            {
+                QFileInfo file(cursorThemePath + "/cursors/" + cursorName);
+                if (file.exists())
+                {
+                    auto cursorImg = CursorImageLoader::getCursorImage(themeBaseName, cursorName, 22);
+                    auto curosrPixmap = QPixmap::fromImage(cursorImg);
+                    themeCursors << curosrPixmap;
+                    break;
+                }
+            }
         }
 
-        auto previewWidget = createPreviewWidget(theme.name,themeCursors,theme.name==currentCursorTheme);
+        auto previewWidget = createPreviewWidget(theme.name, themeCursors, theme.name == currentCursorTheme);
         m_exclusionGroup->addExclusionItem(previewWidget);
         m_cursorVlayout->addWidget(previewWidget);
     }
@@ -118,7 +133,7 @@ void CursorThemePage::onCurrentItemChanged()
     auto id = current->getID();
 
     KLOG_INFO(qLcAppearance) << "cursor theme ui current changed:" << id;
-    if( !AppearanceGlobalInfo::instance()->setTheme(APPEARANCE_THEME_TYPE_CURSOR,id) )
+    if (!AppearanceGlobalInfo::instance()->setTheme(APPEARANCE_THEME_TYPE_CURSOR, id))
     {
         KLOG_WARNING(qLcAppearance) << "set current cursor theme" << id << "failed!";
     }
@@ -126,7 +141,6 @@ void CursorThemePage::onCurrentItemChanged()
     {
         KLOG_INFO(qLcAppearance) << "cursor theme updated:" << id;
     }
-
 }
 
 void CursorThemePage::updateCurrentTheme(QString newCursorTheme)
